89eb604d99b22de75804fe24aebe1b2d00c9ab66
gustavomk482 <gustavomk482@gmail.com>
2013-04-04 20:22:33 -0300

    1º Commit

--- App_Banda_JDBC/src/util/Conexao.java
+++ App_Banda_JDBC/src/util/Conexao.java
@@ -1,3 +1,7 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package util;
 
 import java.sql.Connection;
@@ -5,22 +9,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * Classe responsável por fazer uma conexão ao SGBD e entrega-la pelo método static.
- * A responsabilidade de fechar a conexão não é desta classe. Também não é dela o
- * controle de quantas conexões serão feiras.
- * Importante é configurar o drive jdbc para o SGBD (Postgresql, MySql, Oracle, Java DB, etc),
- * a base de dados em si, o usuário e a senha.
- * Esta classe pode facilmente ser adaptada para ler estas informações de uma arquivo txt ou xml.
- * É importante que o drive jdbc esteja especificado nas bibliotecas deste projeto.
- * @author mertins
+ *
+ * @author Gustavo
  */
 public class Conexao {
-    /**
-     * Método que faz a conexão e o retorna
-     * @return Connection com o SGBD
-     * @throws ClassNotFoundException exception lançada se não for encontrado o drive (verificar as bibliotecas necessárias)
-     * @throws SQLException exception lançada se alguma chamada ao SGBD falhar (sql errado, acesso negado, etc)
-     */
+
     public static Connection get() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection("jdbc:postgresql://localhost/Base_Bandas", "aluno", "qpwo");
